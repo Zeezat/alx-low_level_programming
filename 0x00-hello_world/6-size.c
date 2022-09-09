1c780fc2d423209d32a34e112238b1dcdd2c89fb
Azeezat <tijaniazeezat1997@gmail.com>
2022-09-08 19:43:51 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of achar: %ld byte(S)\n", sizeof(char));
+		printf("Size of an int: %ld byte(s)\n", sizeof(int));
+			printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+				printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+					printf("Size of a float: %ld byte(s)\n", sizeof(float));
+						return (0);
+}
+				
